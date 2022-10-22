3024ab3af3b44b46953b278d269a2dad556c89ef
Владислав Гайденко <vagaidenko@MacBook-Air-Vladislav.local>
2022-10-22 11:26:21 +0700

    исправил модель weather

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	openweather "weather_api/pkg/weather_service/open_weather"
+	openweather "weather_api/pkg/weather_service_client/open_weather"
 )
 
 func main() {
 	weather := openweather.NewOpenWeatherApi(1, "8f7a589bb238eb36173737ebbe1ec8c6", "metric")
-	towns := weather.GetTownStructs()
-	fmt.Println(towns)
+	towns := weather.GetWeatherListStruct(55.75, 37.62)
+	fmt.Println(towns.List[0].Main.Temp)
 }
--- internal/entities/weather.go
+++ internal/entities/weather.go
@@ -5,12 +5,24 @@ type ListWeather struct {
 }
 
 type Weather struct {
-	Main        map[string]float64 `json:"main"`
+	Main        Main               `json:"main"`
 	InfoWeather []InfoWeather      `json:"weather"`
-	Clouds      string             `json:"clouds"`
+	Clouds      map[string]float64 `json:"clouds"`
 	Wind        map[string]float64 `json:"wind"`
-	Visibility  string             `json:"visibility"`
+	Visibility  float64            `json:"visibility"`
 	Date        string             `json:"dt_txt"`
+}
+
+type Main struct {
+	Temp       float64 `json:"temp"`
+	FeelsLike  float64 `json:"feels_like"`
+	TempMin    float64 `json:"temp_min"`
+	TempMap    float64 `json:"temp_map"`
+	Pressure   float64 `json:"pressure"`
+	SeaLevel   float64 `json:"sea_level"`
+	GrindLevel float64 `json:"grind_level"`
+	Humidity   float64 `json:"humidity"`
+	TempKf     float64 `json:"temp_kf"`
 }
 
 type InfoWeather struct {
