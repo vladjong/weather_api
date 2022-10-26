b96d26af28a22e46dd8b4f868f196a0eaa79262e
Владислав Гайденко <vagaidenko@MacBook-Air-Vladislav.local>
2022-10-26 12:46:25 +0700

    Добавил новые сущнности в миграции

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -16,3 +16,24 @@ CREATE TABLE Weathers
     info JSON,
     CONSTRAINT unique_weather UNIQUE(city_id, date)
 );
+
+CREATE TABLE Users
+(
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(30) NOT NULL UNIQUE,
+    password_hash VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Users_lists
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Cities (id) ON DELETE CASCADE NOT NULL,
+    list_id INT REFERENCES Lists (id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE Lists
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    city_id INT REFERENCES Cities (id) NOT NULL
+);
